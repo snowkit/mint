b8c4e09bb7a8c895b787ca47699b446204eb6a1a
underscorediscovery <sven@underscorediscovery.com>
2015-07-16 18:43:07 -0230

    Panel; doc and clean up

--- mint/Panel.hx
+++ mint/Panel.hx
@@ -3,18 +3,26 @@ package mint;
 import mint.Types;
 import mint.Control;
 
+/** Options for constructing a Panel */
 typedef PanelOptions = {
-    > ControlOptions,
-}
 
+    > ControlOptions,
+
+} //PanelOptions
+
+/**
+    A simple blank panel control
+    Additional Signals: none
+*/
+@:allow(mint.ControlRenderer)
 class Panel extends Control {
 
-    @:allow(mint.ControlRenderer)
-        var options: PanelOptions;
+    var options: PanelOptions;
 
     public function new(_options:Dynamic) {
 
         options = _options;
+
         super(options);
 
         canvas.renderer.render(Panel, this);
