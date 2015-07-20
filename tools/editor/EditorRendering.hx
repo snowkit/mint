49f705996247b3789fdbfb40cdbeef9d044dc8b9
underscorediscovery <sven@underscorediscovery.com>
2015-07-20 00:36:23 -0230

    editor; basic editing block in

---
+++ tools/editor/EditorRendering.hx
@@ -0,0 +1,95 @@
+import luxe.Color;
+import luxe.Vector;
+import luxe.Log.*;
+
+class ControlRenderer extends mint.render.Render {
+
+    var bounds : phoenix.geometry.RectangleGeometry;
+    var resizer : phoenix.geometry.RectangleGeometry;
+
+    var color : luxe.Color;
+    var text : luxe.Text;
+    var render : EditorRendering;
+    var rsize = 8;
+
+    public function new( _render:EditorRendering, _control:mint.Control ) {
+
+        render = _render;
+
+        super(_render, _control);
+
+        var rect = new luxe.Rectangle(control.x, control.y, control.w, control.h);
+        var resizerect = new luxe.Rectangle(control.right-rsize, control.bottom-rsize, rsize, rsize);
+        color = new Color(1,1,1,0.8);
+
+        bounds = Luxe.draw.rectangle({ rect:rect, color:color });
+        resizer = Luxe.draw.rectangle({ rect:resizerect, color:color });
+
+        text = new luxe.Text({
+            batcher: render.options.batcher,
+            bounds: new luxe.Rectangle(control.x, control.y, control.w, control.h),
+            color: color,
+            text: '${control.name} ${control.x},${control.y},${control.w},${control.h}',
+            point_size: 14,
+            depth: render.options.depth + control.depth,
+            group: render.options.group,
+            visible: control.visible,
+        });
+
+        control.mouse_input = true;
+        control.onmouseenter.listen(function(_,_){  color.a = 1;  });
+        control.onmouseleave.listen(function(_,_){  color.a = 0.8;  });
+
+    } //new
+
+    override function ondestroy() {
+        bounds.drop();
+        resizer.drop();
+        text.destroy();
+        bounds = null;
+        resizer = null;
+        text = null;
+    }
+
+    override function onbounds() {
+        bounds.set({ x:control.x, y:control.y, w:control.w, h:control.h, color:color });
+        resizer.set({ x:control.right-rsize, y:control.bottom-rsize, w:rsize, h:rsize, color:color });
+        text.bounds = new luxe.Rectangle(control.x, control.y, control.w, control.h);
+        text.text = '${control.name} ${control.x},${control.y},${control.w},${control.h}';
+    }
+
+    override function onvisible( _visible:Bool ) {
+        bounds.visible = text.visible = _visible;
+    }
+
+    override function ondepth( _depth:Float ) {
+        bounds.depth = text.depth = _depth;
+    }
+
+
+}
+
+class EditorRendering extends mint.render.Rendering {
+
+    public var options: luxe.options.RenderProperties;
+
+    public function new( ?_options:luxe.options.RenderProperties ) {
+
+        super();
+
+        options = def(_options, {});
+        def(options.batcher, Luxe.renderer.batcher);
+        def(options.depth, 0);
+        def(options.group, 0);
+        def(options.immediate, false);
+        def(options.visible, true);
+
+    } //new
+
+    override function get<T:mint.Control, T1>( type:Class<T>, control:T ) : T1 {
+        return cast switch(type) {
+            case _: new ControlRenderer(this, cast control);
+        }
+    } //get
+
+} //EditorRendering
--- tools/editor/Main.hx
+++ tools/editor/Main.hx
@@ -2,18 +2,33 @@ import luxe.Color;
 import luxe.Vector;
 import luxe.Input;
 import luxe.Text;
+import luxe.Log.def;
 
-import mint.Types;
+import mint.types.Types;
 import mint.Control;
 import mint.render.luxe.LuxeMintRender;
 import mint.render.luxe.Convert;
+import mint.types.Types.Helper.in_rect;
 
 import mint.layout.margins.Margins;
 
+typedef Ctrl = {
+    id:Int,
+    type:String,
+    tag:luxe.Text,
+    parent:Null<Int>,
+    bounds:luxe.Rectangle,
+    vis:phoenix.geometry.RectangleGeometry 
+};
+
 class Main extends luxe.Game {
 
-    var canvas: mint.Canvas;
-    var render: LuxeMintRender;
+    var ui_batch: phoenix.Batcher;
+    var ui_canvas: mint.Canvas;
+    var ui_render: LuxeMintRender;
+    var editor_canvas: mint.Canvas;
+    var editor_render: EditorRendering;
+
     var layout: Margins;
 
     override function config(config:luxe.AppConfig) {
@@ -30,51 +45,203 @@ class Main extends luxe.Game {
         // Luxe.snow.windowing.enable_vsync(false);
         Luxe.renderer.clear_color.rgb(0x161619);
 
-        new luxe.Sprite({ texture:Luxe.resources.texture('assets/960.png'), centered:false, depth:-1 });
+        ui_batch = Luxe.renderer.create_batcher({ name:'ui', layer:10 });
 
-        render = new LuxeMintRender();
+        ui_render = new LuxeMintRender({ batcher:ui_batch });
+        editor_render = new EditorRendering();
+
+        ui_canvas = new mint.Canvas({ rendering: ui_render, w: Luxe.screen.w, h: Luxe.screen.h });
+        editor_canvas = new mint.Canvas({ rendering: editor_render, w: Luxe.screen.w, h: Luxe.screen.h });
+
         layout = new Margins();
 
-        canvas = new mint.Canvas({
-            renderer: render,
-            x: 0, y:0, w: 960, h: 640
+        new luxe.Sprite({ texture:Luxe.resources.texture('assets/960.png'), centered:false, depth:-1 });
+        var drag = Luxe.camera.add( new util.CameraDrag({name:'drag'}) );
+            drag.zoom_speed = 0.05;
+
+        controls = new Map();
+        create_palette();
+
+    } //ready
+
+    var controls: Map<Int, Ctrl>;
+
+    var down_x = 0;
+    var down_y = 0;
+    var resizing = false;
+    var moving = false;
+    var rsize = 8;
+
+    function ondown(e:mint.types.MouseEvent,c:mint.Control) {
+
+        editor_canvas.modal = c;
+
+        if( in_rect(e.x, e.y, c.right-rsize, c.bottom-rsize, rsize, rsize) ) {
+            resizing = true;
+        } else {
+            moving = true;
+        }
+
+        down_x = Std.int(e.x);
+        down_y = Std.int(e.y);
+    }
+
+    function onmove(e:mint.types.MouseEvent, c:mint.Control) {
+        if(editor_canvas.modal == c) {
+
+            if(moving) {
+                c.set_pos(c.x + (e.x - down_x), c.y + (e.y - down_y));
+            } else if(resizing) {
+                var ww = c.w + (e.x - down_x);
+                var hh = c.h + (e.y - down_y);
+                var resized = false;
+                if(ww >= c.w_min || ww <= c.w_max) { down_x = Std.int(e.x); resized = true; }
+                if(hh >= c.h_min || hh <= c.h_max) { down_y = Std.int(e.y); resized = true; }
+                if(resized) c.set_size(ww, hh);
+            }
+
+            down_x = Std.int(e.x);
+            down_y = Std.int(e.y);
+        }
+    }
+
+    function onup(_,_) {
+        editor_canvas.modal = null;
+        resizing = false;
+        moving = false;
+    }
+
+    function duplicate(c:mint.Control) {
+
+        spawn_control(c.name, c.x+16, c.y+16, c.w, c.h );
+
+    } //duplicate
+
+    function spawn_control(type:String, ?_x:Float, ?_y:Float, ?_w:Float, ?_h:Float) {
+
+        def(_x, Luxe.screen.mid.x-128);
+        def(_y, Luxe.screen.h-128);
+        def(_w, 256);
+        def(_h, 64);
+
+        var control = new mint.Panel({
+            name:'$type',
+            w_min: 16, h_min: 16,
+            parent:editor_canvas,
+            x:_x, y:_y, w:_w, h:_h
         });
 
-    }
+        control.onmousedown.listen(ondown);
+        control.onmousemove.listen(onmove);
+        control.onmouseup.listen(onup);
+
+    } //spawn_control
+
+    var ed_palette: mint.Window;
+    var ed_controls: mint.List;
+
+    function create_palette() {
+
+        ed_palette = new mint.Window({
+            parent: ui_canvas,
+            x: Luxe.screen.w * 0.05,
+            y: Luxe.screen.h * 0.2,
+            w: Luxe.screen.w * 0.2,
+            h: Luxe.screen.w * 0.4,
+            title: 'Controls',
+            closable: false,
+            moveable: false,
+            resizable: false
+        });
+
+        ed_controls = new mint.List({
+            parent: ed_palette,
+            x: 4, y: 30
+        });
+
+        layout.margin(ed_controls, right, fixed, 4);
+        layout.margin(ed_controls, bottom, fixed, 4);
+
+        var list = [
+            'Panel',
+            'Label',
+            'Button',
+            'Image',
+            'List',
+            'Slider',
+            'Scroll',
+            'Checkbox',
+            'Dropdown',
+            'TextEdit',
+            'Progress',
+            'Window',
+        ];
+
+        ed_controls.onselect.listen(function(idx:Int, ctrl:Control, _) {
+            spawn_control(list[idx]);
+        });
+
+        for(name in list) {
+            var label = new mint.Label({
+                parent: ui_canvas,
+                name: 'palette.$name',
+                text: name,
+                text_size: 16,
+                h: 32,
+            });
+            ed_controls.add_item(label, 0, 8);
+            layout.size(label, width, 100);
+        }
+
+    } //create_palette
+
+    override function onwindowsized(event:luxe.Screen.WindowEvent) {
+
+        Luxe.camera.viewport = new luxe.Rectangle(0, 0, Luxe.screen.w, Luxe.screen.h);
+        ui_canvas.set_size(Luxe.screen.w, Luxe.screen.h);
+        editor_canvas.set_size(Luxe.screen.w, Luxe.screen.h);
+
+    } //onwindowsized
 
     override function onmousemove(e) {
 
-        if(canvas!=null) canvas.mousemove( Convert.mouse_event(e) );
+        if(ui_canvas!=null) ui_canvas.mousemove( Convert.mouse_event(e) );
+        if(editor_canvas!=null) editor_canvas.mousemove( Convert.mouse_event(e, Luxe.renderer.batcher.view) );
 
     }
 
     override function onmousewheel(e) {
 
-        if(canvas!=null) canvas.mousewheel( Convert.mouse_event(e) );
+        if(ui_canvas!=null) ui_canvas.mousewheel( Convert.mouse_event(e) );
+        if(editor_canvas!=null) editor_canvas.mousewheel( Convert.mouse_event(e, Luxe.renderer.batcher.view) );
 
     }
 
     override function onmouseup(e) {
 
-        if(canvas!=null) canvas.mouseup( Convert.mouse_event(e) );
+        if(ui_canvas!=null) ui_canvas.mouseup( Convert.mouse_event(e) );
+        if(editor_canvas!=null) editor_canvas.mouseup( Convert.mouse_event(e, Luxe.renderer.batcher.view) );
 
     }
 
     override function onmousedown(e) {
 
-        if(canvas!=null) canvas.mousedown( Convert.mouse_event(e) );
+        if(ui_canvas!=null) ui_canvas.mousedown( Convert.mouse_event(e) );
+        if(editor_canvas!=null) editor_canvas.mousedown( Convert.mouse_event(e, Luxe.renderer.batcher.view) );
 
     } //onmousedown
 
     override function onkeydown(e:luxe.Input.KeyEvent) {
 
-        if(canvas!=null) canvas.keydown( Convert.key_event(e) );
+        if(ui_canvas!=null) ui_canvas.keydown( Convert.key_event(e) );
+        if(editor_canvas!=null) editor_canvas.keydown( Convert.key_event(e) );
 
     } //onkeydown
 
     override function ontextinput(e:luxe.Input.TextEvent) {
 
-        if(canvas!=null) canvas.textinput( Convert.text_event(e) );
+        if(ui_canvas!=null) ui_canvas.textinput( Convert.text_event(e) );
+        if(editor_canvas!=null) editor_canvas.textinput( Convert.text_event(e) );
 
     } //ontextinput
 
@@ -85,19 +252,37 @@ class Main extends luxe.Game {
             return;
         }
 
-        if(canvas!=null) canvas.keyup( Convert.key_event(e) );
+        if(e.keycode == Key.key_d) {
+            if(e.mod.meta || e.mod.ctrl) {
+                if(editor_canvas.focused != null) {
+                    duplicate(editor_canvas.focused);
+                }
+            }
+        }
+
+        if(e.keycode == Key.delete || e.keycode == Key.backspace) {
+            if(editor_canvas.focused != null) {
+                editor_canvas.focused.destroy();
+                editor_canvas.reset_focus();
+            }
+        }
+
+        if(ui_canvas!=null) ui_canvas.keyup( Convert.key_event(e) );
+        if(editor_canvas!=null) editor_canvas.keyup( Convert.key_event(e) );
 
     } //onkeyup
 
     override function onrender() {
 
-        canvas.render();
+        if(ui_canvas!=null) ui_canvas.render();
+        if(editor_canvas!=null) editor_canvas.render();
 
     } //onrender
 
     override function update(dt:Float) {
 
-        if(canvas!=null) canvas.update(dt);
+        if(ui_canvas!=null) ui_canvas.update(dt);
+        if(editor_canvas!=null) editor_canvas.update(dt);
 
     } //update
 
