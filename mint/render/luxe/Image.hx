c910e14190a994c9c17611a129a392af1179436a
underscorediscovery <sven@underscorediscovery.com>
2016-05-12 14:22:18 -0230

    render; luxe; Image; NOTE; changed to be deterministic, see long desc
    The image control previously tried to load the image path but this is a
    problem for a variety of reasons.
    
    For one, all the state changes made to the control either directly or
    by the controls propagating them are ignored until the image loads.
    
    The second is that the control can be destroyed before the image loads.
    
    The third is that the image can fail to load.
    
    The fourth is that creating the control based on the image size isn't
    possible in this model either.
    
    In practice, this was a left over from before luxe resources were
    treated deterministically, and now this falls in line with the rest of
    the engine. You can't use a resource until it is loaded, if you create
    the control with an image that isn't loaded the control will just
    display a white box for now.
    
    For the above issues, giving you the control over failed images, timing
    of creating and destroying, sizes are all in your control rightfully
    now, and, the control can now be deterministic about it's state so it's
    easier to reason about and deal with.
    
    You can use :
    
    Luxe.resources.load_texture(path).then(function(_texture:phoenix.Texture
    ) {
       new mint.Image({
          w:_texture.width, h: _texture.height,
          ...
       })
    
       -- OR --
    
       (cast image.renderer : mint.render.luxe.Image).visual.texture =
    _texture;
    
    });
    
    Whichever works best for your use case.

--- mint/render/luxe/Image.hx
+++ mint/render/luxe/Image.hx
@@ -37,107 +37,93 @@ class Image extends mint.render.Render {
 
         var _opt: LuxeMintImageOptions = image.options.options;
 
-        var get = Luxe.resources.load_texture(image.options.path);
+        var _texture = Luxe.resources.texture(image.options.path);
+        
+        if(_texture != null && _opt.sizing != null) {
 
-        get.then(function(texture:phoenix.Texture){
-
-            if(_opt.sizing != null) {
-
-                switch(_opt.sizing) {
-                        //sets the uv to be the size on the longest edge
-                        //possibly leaving whitespace on the sides (pillarbox) or top (letterbox)
-                    case 'fit': {
-                        if(texture.width > texture.height) {
-                            ratio_h = texture.height/texture.width;
-                        } else {
-                            ratio_w = texture.width/texture.height;
-                        }
-                    } //fit
-
-                        // cover the viewport with the size (possible cropping)
-                    case 'cover': {
-                        var _rx = 1.0;
-                        var _ry = 1.0;
-                        if(texture.width > texture.height) {
-                            _rx = texture.height/texture.width;
-                        } else {
-                            _ry = texture.width/texture.height;
-                        }
-                        _opt.uv = new luxe.Rectangle(0,0,texture.width*_rx,texture.height*_ry);
+            switch(_opt.sizing) {
+                
+                //sets the uv to be the size on the longest edge
+                //possibly leaving whitespace on the sides (pillarbox) or top (letterbox)
+                case 'fit': {
+                    if(_texture.width > _texture.height) {
+                        ratio_h = _texture.height/_texture.width;
+                    } else {
+                        ratio_w = _texture.width/_texture.height;
                     }
+                } //fit
 
-                }
-            }
+                    // cover the viewport with the size (possible cropping)
+                case 'cover': {
+                    var _rx = 1.0;
+                    var _ry = 1.0;
+                    if(_texture.width > _texture.height) {
+                        _rx = _texture.height/_texture.width;
+                    } else {
+                        _ry = _texture.width/_texture.height;
+                    }
+                    _opt.uv = new luxe.Rectangle(0,0,_texture.width*_rx,_texture.height*_ry);
+                } //cover
 
-            visual = new luxe.Sprite({
-                name: control.name+'.visual',
-                batcher: render.options.batcher,
-                centered: false,
-                no_scene: true,
-                texture: texture,
-                pos: new Vector(sx, sy),
-                size: new Vector(sw*ratio_w, sh*ratio_h),
-                depth: render.options.depth + control.depth,
-                visible: control.visible,
-                uv: _opt.uv,
-                color: _opt.color
-            });
+            } //switch sizing
 
-            visual.clip_rect = Convert.bounds(control.clip_with, scale);
+        } //if opt.sizing != null
 
-        }); //get
+        visual = new luxe.Sprite({
+            name: control.name+'.visual',
+            batcher: render.options.batcher,
+            centered: false,
+            no_scene: true,
+            texture: _texture,
+            pos: new Vector(sx, sy),
+            size: new Vector(sw*ratio_w, sh*ratio_h),
+            depth: render.options.depth + control.depth,
+            visible: control.visible,
+            uv: _opt.uv,
+            color: _opt.color
+        });
+
+        visual.clip_rect = Convert.bounds(control.clip_with, scale);
 
     } //new
 
     override function onscale(_scale:Float, _prev_scale:Float) {
         
-        if(visual != null) {
-            visual.clip_rect = Convert.bounds(control.clip_with, _scale);
-            visual.pos.set_xy(sx, sy);
-            visual.size.set_xy(sw*ratio_w, sh*ratio_h);
-        }
+        visual.clip_rect = Convert.bounds(control.clip_with, _scale);
+        visual.pos.set_xy(sx, sy);
+        visual.size.set_xy(sw*ratio_w, sh*ratio_h);
 
     } //onscale
 
     override function onbounds() {
 
-        if(visual != null) {
-            visual.transform.pos.set_xy(sx, sy);
-            visual.geometry_quad.resize_xy(sw*ratio_w, sh*ratio_h);
-        }
+        visual.transform.pos.set_xy(sx, sy);
+        visual.geometry_quad.resize_xy(sw*ratio_w, sh*ratio_h);
 
     } //onbounds
 
     override function ondestroy() {
 
-        if(visual != null) {
-            visual.destroy();
-            visual = null;
-        }
+        visual.destroy();
+        visual = null;
 
     } //ondestroy
 
     override function onclip(_disable:Bool, _x:Float, _y:Float, _w:Float, _h:Float) {
 
-        if(visual != null) {
-            visual.clip_rect = Convert.bounds(control.clip_with, scale);
-        }
+        visual.clip_rect = Convert.bounds(control.clip_with, scale);
 
     } //onclip
 
     override function onvisible( _visible:Bool ) {
 
-        if(visual != null) {
-            visual.visible = _visible;
-        }
+        visual.visible = _visible;
 
     } //onvisible
 
     override function ondepth( _depth:Float ) {
 
-        if(visual != null) {
-            visual.depth = render.options.depth + _depth;
-        }
+        visual.depth = render.options.depth + _depth;
 
     } //ondepth
 
